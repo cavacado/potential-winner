d41f500fda43c15a327e0dc55ce0972bf1e10b97
zl <ng_zhao_loong@tech.gov.sg>
2024-04-09 11:26:33 +0800

    chore: add comments on embed youtube

--- src/app/embed/page.tsx
+++ src/app/embed/page.tsx
@@ -5,6 +5,7 @@ import { sanitiseRawHtml } from "../_utils/sanitise";
 // note that need to enable iframe jsapi if we wanna track
 // video views
 
+// embeds are not working as intended via the manual way.
 const embed = `<iframe width="560" height="315" enablejsapi=1 src="https://www.youtube.com/embed/EngW7tLk6R8?si=VNmsSqAwFWQr9Gri" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
 export default function Embed() {
   return (
